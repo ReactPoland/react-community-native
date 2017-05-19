4e84928ec145a5608b613118b013d264ad6395ff
AdamStrzempa <adstrzempa@gmail.com>
2017-05-19 13:35:44 +0200

    change drawer color and add stacknavigator for navigate in drawer

--- app/routes/drawer/drawerMenu.js
+++ app/routes/drawer/drawerMenu.js
@@ -4,15 +4,17 @@ import {
     StyleSheet,
     Text,
     StatusBar,
-    View
+    View,
+    Image
 } from 'react-native'
 import { COLOR,  ThemeProvider, Toolbar, Drawer, Avatar } from 'react-native-material-ui'
 import Container from './container'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const uiTheme = {
   palette: {
-    primaryColor: '#B38381',
-    accentColor: '#B38381'
+    primaryColor: '#444444',
+    accentColor: '#FFFFFF',
   },
   toolbar: {
     container: {
@@ -31,7 +33,7 @@ export default class DrawerMenu extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      active: 'peopel',
+      active: 'ReactWall',
     };
   }
   render() {
@@ -41,69 +43,71 @@ export default class DrawerMenu extends Component {
           <StatusBar translucent />
           <Toolbar
                     onLeftElementPress={() => this.props.navigation.navigate('DrawerClose')}
-                    centerElement="Menu"
+                    centerElement='Settings'
                   />
           <View style={styles.container}>
-            <Drawer>
-              <Drawer.Header style={{  container: { backgroundColor: '#B38381' },}}>
+            <Drawer style={{ container: { backgroundColor: '#B38381' }}}>
+              <View>
+              </View>
+              <Drawer.Header style={styles.container}>
                 <Text> ICON HERE </Text>
               </Drawer.Header>
-              <Drawer.Section
-                            style={{
-                                label: {color: '#0000ff'}
-                            }}
-                            divider
-                            items={[
-                                {
-                                    value: 'ReactWall',
-                                    active: this.state.active == 'ReactWall',
-                                    onPress: () => {
-                                        this.setState({ active: 'ReactWall' });
-                                        this.props.navigation.navigate('ReactWall');
-                                      },
-                                  },
-                                {
-                                    value: 'ReactMap',
-                                    active: this.state.active == 'ReactMap',
-                                    onPress: () => {
-                                        this.setState({ active: 'ReactMap' });
-                                        this.props.navigation.navigate('ReactMap');
-                                      },
-                                  },
-                                {
-                                    value: 'Best Practices',
-                                    active: this.state.active == 'Best Practices',
-                                    onPress: () => {
-                                        this.setState({ active: 'Best Practices' });
-                                        this.props.navigation.navigate('Best Practices');
-                                      },
-                                  },
-                                {
-                                    value: 'Tutorials',
-                                    active: this.state.active == 'Tutorials',
-                                    onPress: () => {
-                                        this.setState({ active: 'Tutorials' });
-                                        this.props.navigation.navigate('Tutorials');
-                                      },
-                                  },
-                                {
-                                    value: 'Articles',
-                                    active: this.state.active == 'Articles',
-                                    onPress: () => {
-                                        this.setState({ active: 'Articles' });
-                                        this.props.navigation.navigate('Articles');
-                                      },
-                                  },
-                                {
-                                    value: 'Events',
-                                    active: this.state.active == 'Events',
-                                    onPress: () => {
-                                        this.setState({ active: 'Events' });
-                                        this.props.navigation.navigate('Events');
-                                      },
-                                  },
-                            ]}
-                        />
+                <Drawer.Section
+                              style={{
+                                  container: { backgroundColor: '#B38381' }
+                              }}
+                              divider
+                              items={[
+                                  {
+                                      value: 'ReactWall',
+                                      active: this.state.active == 'ReactWall',
+                                      onPress: () => {
+                                          this.setState({ active: 'ReactWall' });
+                                          this.props.navigation.navigate('ReactWall');
+                                        },
+                                    },
+                                  {
+                                      value: 'ReactMap',
+                                      active: this.state.active == 'ReactMap',
+                                      onPress: () => {
+                                          this.setState({ active: 'ReactMap' });
+                                          this.props.navigation.navigate('ReactMap');
+                                        },
+                                    },
+                                  {
+                                      value: 'Best Practices',
+                                      active: this.state.active == 'Best Practices',
+                                      onPress: () => {
+                                          this.setState({ active: 'Best Practices' });
+                                          this.props.navigation.navigate('Best Practices');
+                                        },
+                                    },
+                                  {
+                                      value: 'Tutorials',
+                                      active: this.state.active == 'Tutorials',
+                                      onPress: () => {
+                                          this.setState({ active: 'Tutorials' });
+                                          this.props.navigation.navigate('Tutorials');
+                                        },
+                                    },
+                                  {
+                                      value: 'Articles',
+                                      active: this.state.active == 'Articles',
+                                      onPress: () => {
+                                          this.setState({ active: 'Articles' });
+                                          this.props.navigation.navigate('Articles');
+                                        },
+                                    },
+                                  {
+                                      value: 'Events',
+                                      active: this.state.active == 'Events',
+                                      onPress: () => {
+                                          this.setState({ active: 'Events' });
+                                          this.props.navigation.navigate('Events');
+                                        },
+                                    },
+                              ]}
+                          />
             </Drawer>
           </View>
         </Container>
--- app/routes/drawer/index.js
+++ app/routes/drawer/index.js
@@ -1,12 +1,25 @@
 import DrawerScene from './drawerScene';
-import { DrawerNavigator } from 'react-navigation'
+import { DrawerNavigator, StackNavigator } from 'react-navigation'
 import DrawerMenu from './drawerMenu'
 
-
+//here add sceen for navigation
+// const stackNavigator = StackNavigator({
+//   // ReactWall: { screen: ReactWall },
+//   // ReactMap: { screen: ReactMap },
+//   // BestPractices: { screen: BestPractices },
+//   // Tutorials: { screen: Tutorials },
+//   // Articles: { screen: Articles },
+// }, {
+//   headerMode: 'none'
+// })
 const drawerNavigator = DrawerNavigator({
-  Home: { screen: DrawerScene }
+  Home: { screen: DrawerScene },
+  // Stack: { screen: stackNavigator }
 }, {
-  contentComponent: DrawerMenu
+  contentComponent: DrawerMenu,
+  contentOptions: {
+    inactiveTintColor: '#e91e63'
+  }
 })
 
 

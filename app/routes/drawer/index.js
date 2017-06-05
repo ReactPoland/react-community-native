874e1d57788c8f6a81f08d69934889ce21f1cc60
AdamStrzempa <adstrzempa@gmail.com>
2017-06-05 11:03:55 +0200

    fix settings bug

--- app/routes/drawer/index.js
+++ app/routes/drawer/index.js
@@ -1,33 +1,24 @@
-import { DrawerNavigator, StackNavigator } from 'react-navigation';
+import { DrawerNavigator } from 'react-navigation';
 import { Dimensions } from 'react-native';
 import DrawerMenu from './drawerMenu';
 import ReactWall from '../reactWall/';
 import ReactMap from './routes/reactMap/';
-import Settings from './routes/settings/settingsScene';
+import Settings from './routes/settings/';
 import Articles from './routes/articles/';
 import BestPractices from './routes/bestPractices/';
 import Events from './routes/events/';
 import Tutorials from './routes/tutorials/';
 
 const window = Dimensions.get('window');
-// here add sceen for navigation
-const stackNavigator = StackNavigator({
-    ReactWall: { screen: ReactWall },
+
+const drawerNavigator = DrawerNavigator({
+    Home: { screen: ReactWall },
     ReactMap: { screen: ReactMap },
     Settings: { screen: Settings },
     Articles: { screen: Articles },
     BestPractices: { screen: BestPractices },
     Events: { screen: Events },
     Tutorials: { screen: Tutorials }
-}, {
-    headerMode: 'none',
-    transitionConfig: () => ({
-        screenInterpolator: sceneProps => {}
-    })
-});
-const drawerNavigator = DrawerNavigator({
-    Home: { screen: ReactWall },
-    Stack: { screen: stackNavigator }
 }, {
     contentComponent: DrawerMenu,
     contentOptions: {
